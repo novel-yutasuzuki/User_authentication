7a16d784cb62f020b115dbdd45f1395ac83a063f
Yuta Suzuki <yuta.suzuki.414@gmail.com>
2024-08-08 22:38:04 +0900

    マイグレーション実行

--- app/main.py
+++ app/main.py
@@ -1,27 +1,21 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify
 from models import user
-from config import SessionLocal
+from db import SessionLocal
 
 app = Flask(__name__)
-
-@app.route('/new')
-def user_registration():
-    return render_template('new.html')
 
 @app.route('/create', methods=['POST'])
 def create_user():
     session = SessionLocal()
     try:
-        user = user.User()
-        user.name = request.form['name']
-        user.email = request.form['email']
-        user.password = request.form['password']
-        session.add(user)
+        new_user = user.User()
+        new_user.name = request.form['name']
+        new_user.email = request.form['email']
+        new_user.password = request.form['password']
+        session.add(new_user)
         session.commit()
     finally:
         session.close()
-
-    return render_template('show.html', user = user)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String
-from config import Base, engine
+from db import Base, engine
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User (Base):
@@ -20,7 +20,7 @@ class User (Base):
         self.password_hash = generate_password_hash(password)
 
     # 入力されたパスワードが保存されているハッシュ化したパスワードと一致するか比較する
-    # def verify_password(self, password):
-    #     return check_password_hash(self.password_hash, password)
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     Base.metadata.create_all(engine)
