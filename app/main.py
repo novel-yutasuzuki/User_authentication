14777d798bf94a2c3e95baf572e647c4a61e2e27
Yuta Suzuki <yuta.suzuki.414@gmail.com>
2024-08-14 16:16:53 +0900

    ログイン機能　完成

--- app/main.py
+++ app/main.py
@@ -2,13 +2,17 @@ from flask import Flask, request, jsonify, make_response, Response
 from models.user import User
 from db import SessionLocal, session
 from flask_jwt_extended import JWTManager
-from flask_jwt_extended import (create_access_token, set_access_cookies,unset_access_cookies, verify_jwt_in_request)
+from flask_jwt_extended import (create_access_token, set_access_cookies)
 from werkzeug.security import check_password_hash
 from typing import Optional, Any, Tuple
+import os
 
 
 
 app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY')
+print("JWT_SECRET_KEY:", os.getenv('JWT_SECRET_KEY'))
 
 jwt = JWTManager(app)
 
@@ -27,42 +31,38 @@ def create_user():
         session.close()
 
 @app.route('/login', methods=['POST'])
-# /loginにPOSTメソッドでアクセスした場合のエンドポイント
 def authenticate() -> Tuple[Response, int]:
-# ログインの状況に応じて、レスポンスとHTTPステータスコード(整数)を返す
     # app.logger.info('login started')
-    # ログインスタートというログを残す
     req_data: Any = request.get_json()
-    print()
-    # json形式のデータをキー(req_data)のバリューとしてAnyに代入する
+    print("req_data:", req_data)
     user_name_to_auth: Optional[str] = req_data.get('name')
     email_to_auth: Optional[str] = req_data.get('email')
     password_to_auth: Optional[str] = str(req_data.get('password'))
-    # 上記のカラムのデータをとりだして各変数に代入する
-    email_to_auth: Optional[str] = req_data.get('email')
-    print(user_name_to_auth, email_to_auth, password_to_auth)
+    print("出力:", user_name_to_auth, email_to_auth, password_to_auth)
 
     user = session.query(User).filter(User.name == user_name_to_auth)\
         .filter(User.email == email_to_auth).one_or_none()
-    # Userテーブルに入力された各カラムの入力値と一致するデータがあるか比較し、あればuser変数に代入しなければnoneを返す
-    print(user)
+    # 問題点↑↑↑？
+
+    print("User Name:", user.name)
+    print("User Email:", user.email)
+    print("password_to_auth:", password_to_auth)
+    print("password_hash:", user.password_hash)
     if user and check_password_hash(user.password_hash, password_to_auth):
-        # userが存在し、入力されたパスワードがデータベースに保存されているパスワードのハッシュと一致した場合はTrue
         # app.logger.info('login succeeded')
-        # ログインが成功というメッセージをログに残す
         access_token = create_access_token(
             identity={'name': user.name, 'email': user.email})
-        response = make_response()
-        # nameとemailを含むトークンを作成する
+        response = make_response("成功")
         set_access_cookies(response, access_token)
-        # クッキーに、レスポンス変数とトークンを設定する
+        print("成功")
+        print("access_token:", access_token)
+        print("set_access_cookies:", response.headers.get('Set-Cookie'))
+        print("response:", response)
         return response, 200
-        # レスポンス変数とコードを返す
     else:
+        print("失敗")
         # app.logger.info('login failed')
-        # userが存在せず、入力されたパスワードがデータベースに保存されているパスワードのハッシュと不一致の場合
         return jsonify({'msg': 'Wrong login id or password.'}), 401
-        # json形式でメッセージとコードを返す
 
 if __name__ == "__main__":
     app.run(debug=True)
